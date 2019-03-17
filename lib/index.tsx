9be66dec67569e793ba2e23d1538f710d53e9744
xiaoming <xiaoming@xiaomingdeMBP.lan>
2019-03-18 02:13:26 +0800

    改成import * as ...

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import Icon from './icon/icon';
 
 const fn: React.MouseEventHandler = (e) => {
